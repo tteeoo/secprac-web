e19dd8b70b5bbc48141146d4e821a8bb27645817
GreerPage <greerishere@gmail.com>
2020-06-27 23:27:13 -0400

    reworked errors

--- web/app.py
+++ web/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request
+from werkzeug.exceptions import HTTPException
 import os
 try:
     from .base_utils import readjson, checkjson, writejson
 except: 
-    from base_utils import readjson, checkjson, writejson, errors
+    from base_utils import readjson, checkjson, writejson
 
 app = Flask(__name__)
 path = os.path.dirname(os.path.abspath(__file__))
@@ -16,7 +17,16 @@ def home():
 def create_team():
     if request.method != 'POST':
         return errors.bad_method
-    
+    return 'good boy'
+
+@app.errorhandler(Exception)
+def error(e):
+    code = 500
+    name = "Internal Server Error"
+    if isinstance(e, HTTPException):
+        code = e.code
+        name = " " + e.name
+    return {'error': {'message': name, 'status': code}}
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
--- web/base_utils.py
+++ web/base_utils.py
@@ -18,8 +18,3 @@ def readjson(file):
 def writejson(file, data):
     with open(file, 'w') as e:
         json.dump(data, e)
-
-class errors:
-    bad_method = {'error': {'status': 405, 'message': 'method not allowed'}}
-    not_found = {'error': {'status': 404, 'message': 'not found'}}
-    
