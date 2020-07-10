93fdb87e609d5008cdcc22f7da35a4620935b7e5
Theo Henson <theodorehenson@protonmail.com>
2020-07-09 23:06:18 -0400

    Create pages and styles

--- web/app.py
+++ web/app.py
@@ -26,6 +26,11 @@ if __name__ == '__main__':
 def home():
     return render_template('index.html')
 
+#about page
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 #api endpoints
 
